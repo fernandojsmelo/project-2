01b8c16dad4809f8d03063740b0e6604fd45b3ef
fernandojsmelo <fernandojsmelo@gmail.com>
2024-02-13 09:23:31 -0300

    useState- React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : "";
+
+  const handleClick = () => {
+    setReverse((reverse) => !reverse);
+  }
+
+  const handleIncrement = () => {
+    setCounter((counter) => counter + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img src={logo} className={`App-logo ${reverseClass}`}
+          alt="logo" />
+
+      <h1>Contador: {counter}</h1>
+      <p>
+      <button
+        type='button'
+        onClick={handleClick}
         >
-          Learn React
-        </a>
+          Reverse {reverseClass}
+      </button>
+      </p>
+
+      <p>
+      <button
+        type='button'
+        onClick={handleIncrement}
+        >
+          Increment {counter}
+      </button>
+      </p>
+
       </header>
     </div>
   );
-}
+ }
 
 export default App;
