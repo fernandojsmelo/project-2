9bd7e9e212dcd9c1cec6438197d0cf81d9235df4
fernandojsmelo <fernandojsmelo@gmail.com>
2024-02-14 21:05:04 -0300

    useref - React Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import P from 'prop-types'
 import './App.css';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
-const Post = ({post}) => {
+const Post = ({post, handleClick}) => {
   console.log("Filho Renderizou")
   return (
     <div key={post.id} className='post'>
-      <h1>{post.title}</h1>
+      <h1 
+        style={{ fontSize: '14x' }}
+        onClick={() => handleClick(post.title)}>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   )
@@ -17,27 +19,38 @@ Post.prototype = {
     id: P.number,
     title: P.string,
     body: P.string,
-  })
+  }),
+  onClick: P.func,
 }
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+
   console.log("Pai, renderizou");
   
   // Comnent Did Mount
   useEffect(() => {
-    setTimeout(function() {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
-  }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((r) => r.json())
+    .then((r) => setPosts(r));
   },[]);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current)
+  },[value]);
+
+  const handleClick = (value) => {
+    setValue(value);
+  }
 
   return (
     <div className="App">
       <p>
         <input
+        ref={input}
           type='search'
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -47,7 +60,7 @@ function App() {
         return(
           posts.length > 0 &&
          posts.map((post) => {
-          return (<Post key={post.id} post={post} />)
+          return (<Post key={post.id} post={post} handleClick={handleClick}/>)
          })
         );
       },[posts])}
